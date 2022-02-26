919b2d2df99cd710b69e1f9adc706cdf87f9afab
Hung Phan <phanhung1309@gmail.com>
2022-02-26 15:59:45 +0700

    Navigating between screens

--- screens/CategoryMealsScreen.tsx
+++ screens/CategoryMealsScreen.tsx
@@ -1,10 +1,18 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, Button} from 'react-native';
 import React from 'react';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {MealsParamList, MealsRoutes} from '../routes/MealsNavigationStack';
 
-const CategoryMealsScreen = () => {
+type Props = NativeStackScreenProps<MealsParamList, MealsRoutes.CategoryMeals>;
+
+const CategoryMealsScreen: React.FC<Props> = ({navigation}) => {
   return (
     <View style={styles.screen}>
       <Text>CategoryMealsScreen</Text>
+      <Button
+        title="Go to details"
+        onPress={() => navigation.navigate(MealsRoutes.MealDetail)}
+      />
     </View>
   );
 };
