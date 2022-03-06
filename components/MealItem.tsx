1085044ef43c521271644900cf74f8519cf10a69
Hung Phan <phanhung1309@gmail.com>
2022-03-06 11:53:07 +0700

    Rendering meal lists

---
+++ components/MealItem.tsx
@@ -0,0 +1,93 @@
+import {
+  ImageBackground,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import React from 'react';
+import typography from '../styles/typography';
+
+interface Props {
+  title: string;
+  durations: number;
+  complexity: string;
+  affordability: string;
+  imageUrl: string;
+  onSelect: () => void;
+}
+
+const MealItem: React.FC<Props> = ({
+  title,
+  durations,
+  complexity,
+  affordability,
+  imageUrl,
+  onSelect,
+}) => {
+  return (
+    <View style={styles.mealItem}>
+      <TouchableOpacity onPress={onSelect}>
+        <View>
+          <View style={[styles.mealRow, styles.mealHeader]}>
+            <ImageBackground
+              source={{uri: imageUrl}}
+              style={styles.bgImage}
+              resizeMode="cover">
+              <Text style={styles.title}>{title}</Text>
+            </ImageBackground>
+          </View>
+          <View style={[styles.mealRow, styles.mealDetails]}>
+            <Text style={styles.mealInfo}>{durations}m</Text>
+            <Text style={styles.mealInfo}>{complexity.toUpperCase()}</Text>
+            <Text style={styles.mealInfo}>{affordability.toUpperCase()}</Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default MealItem;
+
+const styles = StyleSheet.create({
+  mealItem: {
+    flex: 1,
+    height: 250,
+    width: '100%',
+    backgroundColor: '#ccc',
+    borderRadius: 8,
+    marginBottom: 32,
+    overflow: 'hidden',
+  },
+  mealRow: {
+    flexDirection: 'row',
+  },
+  mealHeader: {
+    height: '90%',
+  },
+  bgImage: {
+    width: '100%',
+    height: '100%',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  title: {
+    ...typography.primarySemiBold,
+    fontSize: 16,
+    lineHeight: 20,
+    letterSpacing: 0.1,
+    color: 'white',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    paddingHorizontal: 12,
+    paddingVertical: 5,
+  },
+  mealDetails: {
+    paddingHorizontal: 10,
+    justifyContent: 'space-between',
+  },
+  mealInfo: {
+    ...typography.primarySemiBold,
+    marginTop: 4,
+  },
+});
--- screens/CategoryMealsScreen.tsx
+++ screens/CategoryMealsScreen.tsx
@@ -1,22 +1,35 @@
-import {StyleSheet, Text, View, Button} from 'react-native';
-import React from 'react';
+import {StyleSheet, View, FlatList} from 'react-native';
+import React, {useMemo} from 'react';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {MealsParamList, MealsRoutes} from '../routes/MealsNavigationStack';
-import {categoriesData} from '../data/categoriesData';
+import {mealData} from '../data/mealData';
+import MealItem from '../components/MealItem';
 
 type Props = NativeStackScreenProps<MealsParamList, MealsRoutes.CategoryMeals>;
 
 const CategoryMealsScreen: React.FC<Props> = ({navigation, route}) => {
-  const selectedCategory = categoriesData.find(
-    cat => cat.id === route.params.categoryId,
-  );
+  const displayedMeals = useMemo(() => {
+    return mealData.filter(meal =>
+      meal.categoryIds.includes(route.params.categoryId),
+    );
+  }, [route.params.categoryId]);
 
   return (
     <View style={styles.screen}>
-      <Text>CategoryMealsScreen: {selectedCategory.title}</Text>
-      <Button
-        title="Go to details"
-        onPress={() => navigation.navigate(MealsRoutes.MealDetail)}
+      <FlatList
+        showsVerticalScrollIndicator={false}
+        data={displayedMeals}
+        keyExtractor={item => item.id}
+        renderItem={({item}) => (
+          <MealItem
+            title={item.title}
+            durations={item.durations}
+            complexity={item.complexity}
+            affordability={item.affordability}
+            imageUrl={item.imageUrl}
+            onSelect={() => navigation.navigate(MealsRoutes.MealDetail)}
+          />
+        )}
       />
     </View>
   );
@@ -28,6 +41,6 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
+    padding: 15,
   },
 });
