47f239c386e6bca7ad711fdff85296c47d60e13c
Hung Phan <phanhung1309@gmail.com>
2022-03-10 22:28:41 +0700

    Adding bottom tab icon

--- routes/BottomTabNavigator.tsx
+++ routes/BottomTabNavigator.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import MealsNavigationStack from './MealsNavigationStack';
 import FavoritesScreen from '../screens/FavoritesScreen';
+import {colors} from '../styles/colors';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 export enum BottomTabRoutes {
   Meals = 'Meals',
@@ -15,17 +17,42 @@ export type BottomTabParamsList = {
 
 const Tab = createBottomTabNavigator<BottomTabParamsList>();
 
+const tabBarOptions = {
+  tabBarStyle: {
+    backgroundColor: colors.secondary,
+  },
+  tabBarActiveTintColor: 'orange',
+  tabBarInactiveTintColor: 'white',
+};
+
 const BottomTabNavigator = () => {
   return (
     <Tab.Navigator initialRouteName={BottomTabRoutes.Meals}>
       <Tab.Screen
         name={BottomTabRoutes.Meals}
         component={MealsNavigationStack}
-        options={{headerShown: false}}
+        options={{
+          ...tabBarOptions,
+          tabBarIcon: tabInfo => (
+            <Icon name="restaurant" size={24} color={tabInfo.color} />
+          ),
+          headerShown: false,
+        }}
       />
       <Tab.Screen
         name={BottomTabRoutes.Favorites}
         component={FavoritesScreen}
+        options={{
+          ...tabBarOptions,
+          tabBarIcon: tabInfo => (
+            <Icon name="star" size={24} color={tabInfo.color} />
+          ),
+          headerTintColor: 'white',
+          headerStyle: {
+            backgroundColor: colors.secondary,
+          },
+          headerTitleAlign: 'center',
+        }}
       />
     </Tab.Navigator>
   );
